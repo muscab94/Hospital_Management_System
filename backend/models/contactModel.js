0bb370417e6dd0d5603d4d79d932608a0d494a8a
Anas <anascabdi199@gmail.com>
2025-09-15 11:27:11 +0930

    contact backend

---
+++ backend/models/contactModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subject: {
+      type: String,
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    }
+  },
+  { timestamps: true }
+)
+
+const CONTACT_MODEL = mongoose.model("contact", contactSchema)
+export default CONTACT_MODEL
---
+++ backend/routes/contactRouter.js
@@ -0,0 +1,21 @@
+import express from "express"
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+  deleteContact,
+} from "../controllers/contactController.js"
+
+import { authMiddleware, authorizeRoles } from "../middleware/authMiddleware.js"
+
+const router = express.Router()
+
+// PUBLIC route - anyone can send a message
+router.post("/", createContact)
+
+// PROTECTED routes (admin only)
+router.get("/", authMiddleware, authorizeRoles("admin"), getAllContacts)
+router.get("/:id", authMiddleware, authorizeRoles("admin"), getContactById)
+router.delete("/:id", authMiddleware, authorizeRoles("admin"), deleteContact)
+
+export default router
