6dc4ba537019289a0d2cf4b8e674ae9d99386671
omm_ziid <ommdev04@gmail.com>
2025-09-16 15:09:56 +0300

    merging solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import Header from "./components/header";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Routes
 } from "react-router-dom";
 import MainSection from "./components/mainSection";
 import LoginForm from "./components/LoginForm";
@@ -22,42 +22,25 @@ import PatientForm from "./components/PatientForm";
 import PatientDetail from "./components/PatientDetail";
 import ViewAppointment from "./components/ViewAppointment";
 import ReportPage from "./pages/ReportPage";
-function App() {
-=======
-import Header from "./components/header"
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom"
-import MainSection from "./components/mainSection"
-import LoginForm from "./components/LoginForm"
-import RootLayout from "./RootLayout/RootLayout"
-import AuthLayout from "./RootLayout/AuthLayout"
-import DashBoardLayout from "./RootLayout/DashBoardLayout"
-import Dashboard from "./pages/Dashboard"
-import Patients from "./pages/Patients"
-import Appointments from "./pages/Appointments"
-import Staffs from "./pages/Staffs"
-import Reports from "./pages/Reports"
-import MedicalRecords from "./pages/MedicalRecords"
-import ContactForm from "./components/ContactForm"
-import SectionAppointment from "./components/Appointment"
-import About from "./pages/About"
-import Service from "./pages/service"
-import Department from "./pages/Department"
-import Doctor from "./pages/DoctorPages"
-import DoctorPages from "./pages/DoctorPages"
-function App(){
+import ContactForm from "./components/ContactForm";
+import SectionAppointment from "./components/Appointment";
+import About from "./pages/About";
+import Service from "./pages/service";
+import Department from "./pages/Department";
+import DoctorPages from "./pages/DoctorPages";
 
->>>>>>> c3dc669283023e80a11aa760b27402e8a6d3002d
+function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<MainSection />} />
-          <Route path="/contact" element={<ContactForm/>} />
-          <Route path="/appointment" element={<SectionAppointment/>} />
-          <Route path="/About" element={<About/>} />
-          <Route path="/service" element={<Service/>} />
-          <Route path="/Department" element={<Department/>} />
-          <Route path="/doctor" element={<DoctorPages/>} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/appointment" element={<SectionAppointment />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/Department" element={<Department />} />
+          <Route path="/doctor" element={<DoctorPages />} />
         </Route>
         <Route path="/Login" element={<AuthLayout />}>
           <Route index element={<LoginForm />} />
@@ -75,11 +58,11 @@ function App(){
           <Route path="staffs" element={<Staffs />} />
           <Route path="reports" element={<ReportPage />} />
         </Route>
-
       </>
     )
   );
 
   return <RouterProvider router={router} />;
 }
+
 export default App;
